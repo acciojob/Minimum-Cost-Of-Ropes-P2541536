19fca7b88775bfc19cb3446b05d92642a7f284f6
P2541536 <119421978+P2541536@users.noreply.github.com>
2023-02-23 21:36:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-function calculateMinCost() {
+function calculateMinCost(a) {
   //your code here
+	a.preventDefault();
+	var inputElement = doucement.querySelector("input").value;
+	var arr = inputElement.spilt(",");
+	arr.sort(function(a,b){
+		return a-b;
+	});
+	var cost = 0;
+	while(arr.lenght>1){
+		var res = Number(arr[0])+Number(arr[1]);
+		arr.splice(0,2);
+		arr.push(res);
+		cost += res;
+		arr.sort(function(a,b)){
+			return a-b;
+			
+		});
+	}
+	document.getElementById("result").textCotent = cost;
+}
   
   
   
