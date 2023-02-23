bb7669c5eedaf99e6af1584cac73f8aaed68fc2e
P2541536 <119421978+P2541536@users.noreply.github.com>
2023-02-23 21:47:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function calculateMinCost(a) {
+function calculateMinCostToFromRope(a) {
   //your code here
 	a.preventDefault();
 	var inputElement = doucement.querySelector("input").value;
 	var arr = inputElement.spilt(",");
-	arr.sort(function(a,b){
+	arr.sort( function (a,b) {
 		return a-b;
 	});
 	var cost = 0;
-	while(arr.lenght>1){
-		var res = Number(arr[0])+Number(arr[1]);
-		arr.splice(0,2);
+	while(arr.lenght > 1){
+		var res = Number(arr[0]) + Number(arr[1]);
+		arr.splice(0 , 2);
 		arr.push(res);
 		cost += res;
-		arr.sort(function(a,b)){
-			return a-b;
+		arr.sort(function(a , b) {
+			return a - b;
 			
 		});
 	}
